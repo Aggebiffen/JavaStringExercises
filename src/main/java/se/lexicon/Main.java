b4d8e678d1ed28c9af1dbebb632348c5dc231c25
Aggebiffen <aggebrusback@gmail.com>
2023-11-24 13:11:46 +0100

    second commit

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,6 +2,68 @@ package se.lexicon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //exercise_1();
+        //exercise_2();
+        //exercise_3();
+        //exercise_4();
+        //exercise_5();
+        //exercise_6();
+        exercise_7();
+        exercise_8();
+        exercise_9();
+        exercise_10();
+        exercise_11();
     }
+    public static void exercise_1() {
+    //  Java
+    //  0123 answer is 3
+    //  Long example sentence
+    //  0123456 answer is: x
+    }
+    public static void exercise_2() {
+    //  Even longer example sentence
+    //  0123456 answer is: 6
+    }
+    public static void exercise_3() {
+        String String = "Ok this is not as long!";
+        String substring = String.substring(11, String.length() - 1);
+        System.out.println(substring);
+    }
+    public static void exercise_4() {
+        String StringScreaming = "CAPS EQUALS SCREAMING";
+        StringScreaming = StringScreaming.toLowerCase();
+        System.out.println(StringScreaming);
+        StringScreaming = StringScreaming.toUpperCase();
+        System.out.println(StringScreaming);
+
+    }
+    public static void exercise_5() {
+    String text = "Java is the worst programming language!";
+        System.out.println(text.replace("worst", "best"));
+
+    }
+    public static void exercise_6() {
+    String trim = "\tJ\ta\tv\ta\t";
+    // System.out.println(trim.trim());
+    // Answer: J	a	v	a
+
+    }
+    public static void exercise_7() {
+
+    }
+    public static void exercise_8() {
+
+    }
+    public static void exercise_9() {
+
+    }
+    public static void exercise_10() {
+
+    }
+    public static void exercise_11() {
+
+    }
+
+
 }
